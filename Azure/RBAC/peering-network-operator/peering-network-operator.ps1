d961df2a367aff1d5bb2fd03e1497f0da087995a
Oliveira1 <admin.oliveira1@softwareone.com>
2018-10-01 14:57:12 -0300

    updated scripts description

--- Azure/RBAC/peering-network-operator/peering-network-operator.ps1
+++ Azure/RBAC/peering-network-operator/peering-network-operator.ps1
@@ -1,14 +1,14 @@
 ##############################################################
-# Title: Create Resource Move Operator
+# Title: Create Peering Network Operator
 # Version: 1.0
 # Author: Carlos Oliveira
-# Last Modified: 2018-09-20
-# Description: This role restricts contributors from deleting resource locks from resource groups. Then, they can create a lock, but they can't delete this lock.
+# Last Modified: 2018-10-01
+# Description: This role allows the user to perform Network Peering related operations in a single active directory
 # 
 ### How to use this Powershell
 # 0. Open Azure Cloud Shell at https://shell.azure.com/
 # 1. Select Powershell (Preview)
-# 2. Import file resource-move-operator.json to /home/<yourname>/
+# 2. Import file peering-network-operator.json to /home/<yourname>/
 # 3. go to your /home/ page using
 # > cd $home
 # 4. Create a folder called azure-templates and enter on it Use the following commands: 
@@ -17,4 +17,4 @@
 # 5. Run the cmdlet below
 ##############################################################
 
-New-AzureRmRoleDefinition -InputFile "contributor-lock.json"
+New-AzureRmRoleDefinition -InputFile "peering-network-operator.json"
