7856537901f7678b4895171b8128ac93baddd70d
Henrique <henriquebrisola@gmail.com>
2018-10-26 15:04:01 -0300

    Update Unattended login.ps1
    correction

--- Azure/ARM/Unattended login.ps1
+++ Azure/ARM/Unattended login.ps1
@@ -2,7 +2,7 @@
 login-azurermaccount
 $subscription=
 select-azurermsubscription -subscription $subscription
-export-AzureRmProfile -Path "D:\Google Drive\Cloud\Azure\credential.json"
+export-AzureRmProfile -Path "C:\credential.json"
 
 #usage - use this command to login whithout prompt and then you can run any automated script
-import-AzureRmProfile -Path "D:\Google Drive\Cloud\Azure\credential.json"
+import-AzureRmContext -Path "C:\credential.json"
