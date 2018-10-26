9e424633cadd4cd62518a4f7722ec03d7e60a536
Henrique <henriquebrisola@gmail.com>
2018-10-26 16:17:26 -0300

    Add files via upload
    ps1 to change disk type from SSD Premium to SSD Stardard, HDD or vice versa

---
+++ Azure/Storage/disk type change.ps1
@@ -0,0 +1,15 @@
+﻿$storageType = 'Standard_LRS'  # Premium_LRS, StandardSSD_LRS, Standard_LRS
+
+foreach ($vm in $vms) {
+    $osdisk = $vm.storageprofile.osdisk.name
+    $diskUpdateConfig = New-AzureRmDiskUpdateConfig -AccountType $storageType 
+    Update-AzureRmDisk -DiskUpdate $diskUpdateConfig -ResourceGroupName $vm.ResourceGroupName `
+    -DiskName $osdisk
+
+    $datadisks = $vm.StorageProfile.DataDisks
+    foreach ($disk in $datadisks){
+        $diskUpdateConfig = New-AzureRmDiskUpdateConfig -AccountType $storageType 
+        Update-AzureRmDisk -DiskUpdate $diskUpdateConfig -ResourceGroupName $vm.ResourceGroupName `
+        -DiskName $disk.Name
+    }
+}
